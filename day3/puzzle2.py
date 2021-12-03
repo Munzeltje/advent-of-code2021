1617d39b66d88d02216f1d5e9ad0193a7a4d5fe6
vivian <vivianvanoijen@protonmail.com>
2021-12-03 16:07:41 +0100

    eliminate duplicate code

--- day3/puzzle2.py
+++ day3/puzzle2.py
@@ -9,32 +9,34 @@ with open("../input.txt") as file:
         ]
     )
 
-columns = diagnostic.shape[1]
 
-oxygen_generator = np.copy(diagnostic)
-for i in range(columns):
-    frequencies = np.bincount(oxygen_generator[:, i])
-    if frequencies[0] == frequencies[1]:
-        oxygen_generator = oxygen_generator[np.where(oxygen_generator[:, i] == 1)]
-    else:
-        oxygen_generator = oxygen_generator[
-            np.where(oxygen_generator[:, i] == frequencies.argmax())
-        ]
-    if oxygen_generator.shape[0] == 1:
-        break
+def find_rating(diagnostic, rating_type):
+    columns = diagnostic.shape[1]
+    rating_values = np.copy(diagnostic)
+    for i in range(columns):
+        frequencies = np.bincount(rating_values[:, i])
+        if frequencies[0] == frequencies[1]:
+            rating_values = rating_values[np.where(rating_values[:, i] == rating_type)]
+        else:
+            if rating_type == 0:
+                rating_values = rating_values[
+                    np.where(rating_values[:, i] == frequencies.argmin())
+                ]
+            elif rating_type == 1:
+                rating_values = rating_values[
+                    np.where(rating_values[:, i] == frequencies.argmax())
+                ]
+        if rating_values.shape[0] == 1:
+            break
+    return rating_values
 
-co2_scrubber = np.copy(diagnostic)
-for i in range(columns):
-    frequencies = np.bincount(co2_scrubber[:, i])
-    if frequencies[0] == frequencies[1]:
-        co2_scrubber = co2_scrubber[np.where(co2_scrubber[:, i] == 0)]
-    else:
-        co2_scrubber = co2_scrubber[
-            np.where(co2_scrubber[:, i] == frequencies.argmin())
-        ]
-    if co2_scrubber.shape[0] == 1:
-        break
 
-OXYGEN_GENERATOR_RATING = int("".join(map(str, oxygen_generator[0])), 2)
-CO2_SCRUBBER_RATING = int("".join(map(str, co2_scrubber[0])), 2)
+OXYGEN_GENERATOR = 1
+CO2_SCRUBBER = 0
+OXYGEN_GENERATOR_RATING = int(
+    "".join(map(str, find_rating(diagnostic, OXYGEN_GENERATOR)[0])), 2
+)
+CO2_SCRUBBER_RATING = int(
+    "".join(map(str, find_rating(diagnostic, CO2_SCRUBBER)[0])), 2
+)
 print(OXYGEN_GENERATOR_RATING * CO2_SCRUBBER_RATING)
